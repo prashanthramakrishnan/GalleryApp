d28f26f933648cc7b9b86b4ffd97fb1d2f24c341
Prashanth Ramakrishnan <prashanth_r03@yahoo.co.in>
2019-06-20 14:51:09 +0530

    - Remove Picasso and use Glide since Picasso was throwing OOM error - Show images only from a particular folder - Ability to view the individual images - Add Firebase to the app

---
+++ app/src/main/java/com/prashanth/galleryapp/ui/activity/ViewImageActivity.java
@@ -0,0 +1,5 @@
+package com.prashanth.galleryapp.ui.activity;
+
+public class ViewImageActivity {
+
+}
---
+++ app/src/main/java/com/prashanth/galleryapp/ui/adapter/AlbumAdapter.java
@@ -0,0 +1,5 @@
+package com.prashanth.galleryapp.ui.adapter;
+
+public class AlbumAdapter {
+
+}
